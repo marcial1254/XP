736ca0d79075a75cb478a9e6e551b865e19b34e8
AlePingu <agfpingu@gmail.com>
2014-11-17 10:57:36 -0400

    la clase para hacer querys creada, no es version final

--- database/Constantes.java
+++ database/Constantes.java
@@ -10,8 +10,7 @@ public class Constantes {
     /** Postgres datos para la conexion */
     
     public static final String POSTGRESQL_DRIVER = "org.postgresql.Driver";
-    public static final String POSTGRESQL_DBURL = 
-            "jdbc:postgresql://localhost:5432/conferencia";
+    public static final String POSTGRESQL_DBURL = "jdbc:postgresql://localhost:5432/conferencia";
     public static final String POSTGRESQL_USER = "sis2";
     public static final String POSTGRESQL_PASS = "conferencia";
 
---
+++ database/ControladorConsultas.java
@@ -0,0 +1,10 @@
+public class ControladorConsultas
+{
+	//private PostgresConn conector;
+
+	//los metodos de PostgresConn son estaticos
+	public ControladorConsultas()
+	{
+		//conector = new PostgresConn()
+	}
+}
